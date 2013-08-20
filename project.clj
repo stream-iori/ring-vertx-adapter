b401e99b4d6639c89d2c822844d9895a95bd2096
stream1984 <stream1984@gmail.com>
2013-08-20 11:09:25 +0800

    change the version of api

--- project.clj
+++ project.clj
@@ -4,11 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[ring/ring-core "1.2.0"]
-                 [io.vertx/clojure-api "1.0.0-SNAPSHOT"]]
+                 [io.vertx/clojure-api "0.2.0-SNAPSHOT"]]
 
   :profiles
   {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                         [compojure "1.1.5"]
-                        [io.vertx/vertx-platform "2.0.0-final"]
-                        [io.vertx/vertx-core "2.0.0-final"]
                         [clj-http "0.6.4"]]}})
